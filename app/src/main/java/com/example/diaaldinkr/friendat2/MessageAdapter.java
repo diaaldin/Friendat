5dff45505796d88ad72bc77d77fe00299e5423b4
diaaldin <dia81994@gmail.com>
2019-02-03 11:37:25 +0200

    finish the voice messages

--- app/src/main/java/com/example/diaaldinkr/friendat2/MessageAdapter.java
+++ app/src/main/java/com/example/diaaldinkr/friendat2/MessageAdapter.java
@@ -300,7 +300,6 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
     public  String milliSecondsToTimer(long milliseconds) {
         String finalTimerString = "";
         String secondsString = "";
-
         // Convert total duration into time
         int hours = (int) (milliseconds / (1000 * 60 * 60));
         int minutes = (int) (milliseconds % (1000 * 60 * 60)) / (1000 * 60);
@@ -309,16 +308,13 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
         if (hours > 0) {
             finalTimerString = hours + ":";
         }
-
         // Prepending 0 to seconds if it is one digit
         if (seconds < 10) {
             secondsString = "0" + seconds;
         } else {
             secondsString = "" + seconds;
         }
-
         finalTimerString = finalTimerString + minutes + ":" + secondsString;
-
         // return timer string
         return finalTimerString;
     }
